85c4119982c91771f502365ae2d4499cd97e378c
ZaryonDE <Zaryon@gmx.de>
2025-11-02 22:38:45 +0100

    changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ tasks.named("build") {
 }
 
 base {
-    archivesName.set("BetterAnimalFeeding-1.21.6-Fabric")
+    archivesName.set("BetterAnimalFeeding-1.21.7-Fabric")
 }
 
 repositories {
@@ -48,10 +48,10 @@ repositories {
 }
 
 dependencies {
-    minecraft("com.mojang:minecraft:1.21.6")
-    mappings("net.fabricmc:yarn:1.21.6+build.1:v2")
+    minecraft("com.mojang:minecraft:1.21.7")
+    mappings("net.fabricmc:yarn:1.21.7+build.1:v2")
     modImplementation("net.fabricmc:fabric-loader:0.17.2")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.128.2+1.21.6")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.129.0+1.21.7")
 
     modImplementation("com.terraformersmc:modmenu:15.0.0") {
         isTransitive = false
@@ -90,7 +90,7 @@ modrinth {
     versionNumber.set(version.toString())
     versionType.set("release")
     uploadFile.set(tasks.remapJar)
-    gameVersions.addAll(listOf("1.21.6"))
+    gameVersions.addAll(listOf("1.21.7"))
     loaders.add("fabric")
     changelog.set(System.getenv("GITHUB_RELEASE_BODY") ?: "Automatischer Release-Build")
     dependencies {
